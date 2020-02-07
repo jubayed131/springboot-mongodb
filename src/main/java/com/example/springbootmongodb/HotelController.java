f54d7e6fc64238e2334ef0d6e3c2d7cf8f062919
Jubayed <$hahNewaz1Jub$hant1>
2020-02-07 01:36:16 +0100

    Hotel controller for CRUD operation finshed

---
+++ src/main/java/com/example/springbootmongodb/HotelController.java
@@ -0,0 +1,39 @@
+package com.example.springbootmongodb;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hotels")
+public class HotelController {
+    private HotelRepository hotelRepository;
+
+    public HotelController(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    }
+    @GetMapping("/all")
+    public List<Hotel> getAll(){
+       List<Hotel> hotels=this.hotelRepository.findAll();
+       return hotels;
+    }
+
+    @PutMapping
+    public void insert(@RequestBody Hotel hotel){
+        this.hotelRepository.insert(hotel);
+    }
+
+    @PostMapping
+    public void update(@RequestBody Hotel hotel){
+        this.hotelRepository.save(hotel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") String id){
+        this.hotelRepository.deleteById(id);
+    }
+
+}
+
+
+
